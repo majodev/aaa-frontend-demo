01a86ce7d15ebe89b3b7dd1f829ed7d8ee916293
Mario Ranftl <mario.ranftl@allaboutapps.at>
2017-10-20 15:35:58 +0200

    access raw beer information in the detail route

--- src/components/beers/BeerDetailRoute.tsx
+++ src/components/beers/BeerDetailRoute.tsx
@@ -1,11 +1,29 @@
 import * as React from "react";
 import { RouteComponentProps } from "react-router-dom";
 import styled from "styled-components";
+import { observer } from "mobx-react";
+
+import FullScreenProgress from "../common/FullScreenProgress";
+import beerState from "../../state/beersState";
 
 interface IState { }
 
+@observer
 export default class Component extends React.Component<RouteComponentProps<{ id: string }>, IState> {
+
+    componentDidMount() {
+        beerState.selectBeer(Number.parseInt(this.props.match.params.id));
+    }
+
     render() {
-        return (<small>beerdetail: id {this.props.match.params.id}</small>);
+
+        const { selectedBeer, loading } = beerState;
+
+        return (
+            <div>
+                <FullScreenProgress show={loading} />
+                <pre>{JSON.stringify(selectedBeer, null, 2)}</pre>
+            </div>
+        );
     }
 }
--- src/state/beersState.ts
+++ src/state/beersState.ts
@@ -1,4 +1,6 @@
 import { observable, action } from "mobx";
+import * as _ from "lodash";
+
 import { IBeer } from "./IBeer";
 
 class BeersState {
@@ -6,11 +8,12 @@ class BeersState {
     @observable loading: boolean = false;
     @observable beers: IBeer[] = [];
     @observable remainingRequests: number = 0;
+    @observable selectedBeer: IBeer | null = null;
 
     @action loadBeers = async () => {
         if (this.loading || this.beers.length > 0) {
             // noop if we are loading or have already loaded our beers
-            return; 
+            return;
         }
 
         this.loading = true;
@@ -19,6 +22,31 @@ class BeersState {
         this.beers = await res.json();
         this.remainingRequests = Number.parseInt(res.headers.get("x-ratelimit-remaining") as string);
 
+        this.loading = false;
+    }
+
+    @action selectBeer = async (id: number) => {
+
+        const beer = _.find(this.beers, { id });
+
+        if (beer) {
+            // beer was already loaded, noop.
+            this.selectedBeer = beer;
+            return;
+        }
+
+        this.loading = true;
+        const res = await fetch(`https://api.punkapi.com/v2/beers/${id}`);
+        const [ newBeer ] = await res.json();
+        this.remainingRequests = Number.parseInt(res.headers.get("x-ratelimit-remaining") as string);
+        
+        // update our collection.
+        this.beers = [
+            ...this.beers,
+            newBeer
+        ];
+
+        this.selectedBeer = newBeer;
         this.loading = false;
     }
 
