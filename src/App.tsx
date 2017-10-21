ac3f093534dd0071a47a87620adb3a2458c7fadc
Mario Ranftl <mario.ranftl@allaboutapps.at>
2017-10-21 20:17:30 +0200

    switch to hashRouter

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import * as React from "react";
 import MuiThemeProvider from "material-ui/styles/MuiThemeProvider";
 import * as injectTapEventPlugin from "react-tap-event-plugin";
 import { IntlProvider } from "react-intl";
-import { BrowserRouter as Router, Route, } from "react-router-dom";
+import { HashRouter as Router, Route, } from "react-router-dom";
 
 import muiTheme from "./muiTheme";
 import baseLocale from "./i18n/en";
