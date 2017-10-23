43ab3b1923799dfdd0f417b11b01e6e66d85d2a4
Mario Ranftl <mario.ranftl@allaboutapps.at>
2017-10-23 16:12:31 +0200

    beers ul

--- src/components/beers/Beers.tsx
+++ src/components/beers/Beers.tsx
@@ -1,15 +1,55 @@
 import * as React from "react";
 import styled from "styled-components";
 
+import { IBeer } from "./IBeer";
 import * as i18n from "../../i18n/util";
 
 interface IProps { }
-interface IState { }
+interface IState {
+    remainingRequests: number;
+    beers: IBeer[];
+}
 
 export default class Component extends React.Component<IProps, IState> {
+
+    constructor(props: IProps) {
+        super(props);
+
+        this.state = {
+            remainingRequests: -1,
+            beers: []
+        };
+    }
+
+    componentDidMount() {
+        this.fetchBeers();
+    }
+
+    async fetchBeers() {
+        const res = await fetch("https://api.punkapi.com/v2/beers");
+
+        const remainingRequests = Number.parseInt(res.headers.get("x-ratelimit-remaining") as string);
+        const json = (await res.json()) as IBeer[];
+
+        this.setState({
+            remainingRequests,
+            beers: json
+        });
+    }
+
     render() {
         return (
-            <h1>Beers</h1>
+            <div>
+                <h1>Beers {this.state.remainingRequests}</h1>
+                <ul>
+                    {this.state.beers.map((beer) => {
+                        return (
+                            <li key={beer.id}>{beer.name}</li>
+                        );
+                    })}
+                </ul>
+            </div>
         );
     }
 }
+
